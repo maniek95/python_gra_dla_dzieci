0973e663e00e2796fb737270d8df5d0fb9640fdb
student <marta.manka.95@gmail.com>
2017-02-10 22:50:00 +0100

    poprawa wygladu

---
+++ projekt/README.md
@@ -0,0 +1,22 @@
+# python_gra_dla_dzieci
+Projekt przygotowany na przedmiot "Programowanie w języku Python" na 3 roku kierunku "Informatyka Stosowana"
+
+Projekt ma z załozenia być przeznaczony dla uczniów początkowych klas szkoły podstawowej. 
+Ma on na celu sprawdzanie biegłości w operowaniu na podstawowych operacjach matematycznych. 
+
+W poczatkowych klasach SP uczniowie doskonala znajomosc dodawania i odejmowania w zakresie 100. Nie poznają oni ułamków ( choc powinni, ale i na to przyjdzie czas) ani liczb ujemnych dlatego moje losowanie odbywa sie tylko i wyłącznie na zbiorze liczb naturalnych. Poziom trudny jest przeznaczony dla troszkę starszych użytkowników.
+
+Gra jest podzielona na 4 poziomy trudności.
+Banalny ma zakres losowania 1-10,
+łatwy: 1-20,
+średni: 1-100 ,
+a trudny jest od 1 do 1000
+
+Gra jest stworzona za pomocą idle, ekran pożegnalny jest stworzony z wykorzystaniem biblioteki wbudowanej Pythona: Tkinter, oraz modulem do ladowania obrazkow ( ImageTk)
+
+Aby rozpocząć zabawe należy uruchomić najpierw plik konsolowa.py
+Ekran pożegnalny znajduje sie w pliku pa.py
+
+Na zakończenie pracy aplikacja zapisuje wynik uzyskany przez użytkownika do pliku oraz wyświetla bardzo smutnego Ciasteczkowego Potwora.
+
+
---
+++ projekt/konsolowa.py
@@ -0,0 +1,158 @@
+# wersja konsolkowa
+
+import random
+import math
+import sys
+
+
+
+class Dzialanie:
+    
+    operacja = {
+    "dodawanie": lambda x,y: x+y,
+    "odejmowanie": lambda x,y: x-y,
+    "mnozenie": lambda x,y: x*y,
+    "dzielenie": lambda x,y: x/y
+    }
+    
+    licznik=0
+    def komunikat(self,licznik):
+        if dz.licznik==2:
+           print "\n\n\nOby tak dalej!\n\n\n"
+        elif dz.licznik==10:
+            print "\n\n\nPedzisz jak burza! Wow! Super Ci idzie!\n\n\n"
+        elif dz.licznik==20:
+            print "\n\n\nSUPER! Jestes prawdziwym mistrzem!\n\n\n"
+        elif dz.licznik==100:
+            print "\n\n\nNIE WIERZE! JESTEM KROLEM MATEMATYKI!\n\n\n"
+        elif dz.licznik==1000:
+            print "\n\n\nTo nie mozliwe ale jednak. Nie ma lepszych od Ciebie :)\n\n\n "
+
+    def dod(self,x,y):
+        wynik=self.operacja["dodawanie"](x,y)
+        napis= "ile jest: " + str(x) + " + " + str(y) + "???"
+        print napis
+        odp=input()
+        if wynik==odp:
+           self.licznik +=1
+           print '\nSUPER! POPRAWNA ODPOWIEDZ!\n' 
+           print "\nStan twoich punktow :)) : " + str(self.licznik)
+        else:
+           print "\nSPROBUJ JESZCZE RAZ :) NA PEWNO CI SIE UDA! stan twoich punktow to: "  + str(self.licznik)
+           self.dod(x,y)
+            
+    def od(self,x,y):
+        wynik=self.operacja["odejmowanie"](x,y)
+        napis= "ile jest: " + str(x) + " - " + str(y) + "???" 
+        print napis
+        odp=input()
+        if wynik==odp:
+           self.licznik +=1
+           print 
+           print "\nStan twoich punktow :)) : " + str(self.licznik)
+        else:
+           print "\nSPROBUJ JESZCZE RAZ :) NA PEWNO CI SIE UDA!\n stan twoich punktow to: "  + str(self.licznik)
+           self.od(x,y)
+    def mnoz(self,x,y):
+        wynik=self.operacja["mnozenie"](x,y)
+        napis= "ile jest: " + str(x) + " * " + str(y) + "???" 
+        print napis
+        odp=input()
+        if wynik==odp:
+           self.licznik +=1
+           print '\nSUPER! POPRAWNA ODPOWIEDZ!\n' 
+           print "\nStan twoich punktow :))  : " + str(self.licznik)
+        else:
+           print "\n\nSPROBUJ JESZCZE RAZ :) NA PEWNO CI SIE UDA! \nstan twoich punktow to: "  + str(self.licznik)
+           self.mnoz(x,y)
+        
+    def dziel(self,x,y):
+        
+        wynik=self.operacja["dzielenie"](x,y)
+        napis= "ile jest: " + str(x) + " : " + str(y) + "???" 
+        print napis
+        odp=input()
+        if wynik==odp:
+           self.licznik +=1
+           print '\n\nSUPER! POPRAWNA ODPOWIEDZ!\n\n' 
+           print "\nStan twoich punktow :)  : " + str(self.licznik)
+        else:
+           print "\n\nSPROBUJ JESZCZE RAZ :) NA PEWNO CI SIE UDA! \nstan twoich punktow to: "  + str(self.licznik)
+           self.dziel(x,y)
+    def wybor_operacji(self,x,y):
+
+        print "\nWybierz dzialanie \n 1.\tDodawanie \n 2.\tOdejmowanie \n 3.\tMnozenie \n 4.\tDzielenie \n"
+        b=input()
+        if  b== 1:
+             self.dod(x,y)
+             
+                 
+        elif b== 2:
+            tmp=0
+            if (x-y)<0:
+              x=tmp
+              x=y
+              tmp=y 
+            self.od(x,y)
+             
+            
+        elif b== 3:
+             self.mnoz(x,y)
+            
+        elif b== 4:
+             tmp=0
+             if y>x:
+               x=tmp
+               x=y
+               tmp=y 
+             x=x+x%y
+             self.dziel(x,y)
+        else:
+            print '\n\nwybierz opcje jeszcze raz\n\n'
+            self.wybor_operacji(self,x,y)
+    def wybor(self,us):
+         
+         print "\nWybierz poziom trudnosci \n 1.\tbanalny \n 2.\tlatwy \n 3.\tsredni \n 4.\ttrudny \n\n\n5.\tKONIEC ZABAWY :) \n"
+        
+         a=input()
+      
+         if a== 1:
+            b=random.randint(1,10)
+            c=random.randint(1,10)
+            self.wybor_operacji(b,c)
+                  
+
+         elif a== 2:
+             
+             b=random.randint(1,20)
+             c=random.randint(1,20)
+             self.wybor_operacji(b,c)
+             
+         elif a== 3:
+             b=random.randint(1,100)
+             c=random.randint(1,100)  
+             self.wybor_operacji(b,c)  
+         elif a== 4:
+             b=random.randint(1,1000)
+             c=random.randint(1,1000)  
+             self.wybor_operacji(b,c)
+         elif a==5:
+             execfile("pa.py")
+             file=open("wyniki.txt","ar+")
+             file.write(str(us) + "  uzyskal/a : " + str(self.licznik) + "\n")
+             file.close()
+             
+             sys.exit()
+         else:
+             print '\n\nzla opcja! \n\n'
+             self.wybor()
+   
+
+
+dz=Dzialanie()
+
+user=raw_input("CZESC! WPISZ SWOJE IMIE: \n ")
+while(1):
+    dz.wybor(user)
+    dz.komunikat(dz.licznik)
+
