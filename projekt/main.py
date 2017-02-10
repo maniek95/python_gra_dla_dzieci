0f224466b1cb5786701805641781e86059350e27
student <marta.manka.95@gmail.com>
2017-02-10 23:40:49 +0100

    zmiany

---
+++ projekt/main.py
@@ -0,0 +1,43 @@
+
+from Tkinter import *
+import sys
+from PIL import ImageTk, Image
+import os
+
+
+okno=Tk()
+
+def wyjscie():
+    from pa import *
+    okno.close()
+def rozpocznij():
+   
+    from konsolowa import *
+    okno.close()
+
+okno.title("Nauka matematyki dla uczniow szkol podstawowych")
+okno.geometry("800x800")
+#okno.configure(background='grey')
+
+button=Button(okno,text="Zakoncz zabawe!",command=wyjscie, bg="red", fg="white",font=("Arial",24,"italic"))
+
+button2=Button(okno,text="Rozpocznij przygode!", command=rozpocznij, bg="green", fg="white",font=("Arial",24,"italic"))
+
+label=Label(okno,text="\n\nWitaj w magicznej krainie matematyki \n \n Aby rozpoczac zabawe nacisnij przycisk ROZPOCZNIJ  PRZYGODE! \n Aby wyjsc z programu wcisnij KONIEC ZABAWY!\n\n\n", font=("Arial",15,"bold"),bg="grey",fg="white")
+
+img = ImageTk.PhotoImage(Image.open("CountCookies.jpg"))
+panel = Label(okno, image = img)
+
+
+#label.pack(fill=X)
+#label.pack(fill=Y)
+label.pack(side="top")
+panel.pack(side = "top", fill = "both", expand = "yes")
+button2.pack(side = "left", fill="x", expand="yes")
+
+print "\n"
+button.pack(side = "right",fill="x" , expand="yes")
+
+print "\n\n\n\n"
+okno.mainloop()
+
--- projekt/pa.py
+++ projekt/pa.py
@@ -1,14 +1,19 @@
-import Tkinter
+from Tkinter import *
 from PIL import ImageTk, Image
 
-okno=Tkinter.Tk()
-okno.title("Do zobaczenia!")
-okno.geometry("800x800")
+def funkcja(okno):
+    
+    okno.title("Do zobaczenia!")
+    okno.geometry("800x800")
+
+    Opis=Label(okno,text="\n\nJuz dzisiaj nic nie policzymy :( \n Wroc jeszcze kiedys!\n\n\n", font=("Arial",15,"bold"),bg="grey",fg="white")
+    Opis.pack(side="top")
+    
 
 
-img = ImageTk.PhotoImage(Image.open("cookiemonster4.jpg"))
-panel = Tkinter.Label(okno, image = img)
-Opis=Tkinter.Label(okno,text="\n\nJuz dzisiaj nic nie policzymy :( \n Wroc jeszcze kiedys!\n\n\n", font=("Arial",15,"bold"),bg="grey",fg="white")
-Opis.pack(side="top")
+okno=Tk()
+funkcja(okno)
+img2 = ImageTk.PhotoImage(Image.open("cookiemonster4.jpg"))
+panel = Label(okno, image = img2)
 panel.pack(side = "bottom", fill = "both", expand = "yes")
 okno.mainloop()
